97b4f9f70641296e93a919408d69e9d91b5b7853
Niklas Werner <vveil.socialmedia@gmail.com>
2021-01-18 07:47:07 +0100

    Added GameManager Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject turret;
+
+    public void instantiateTurret(Vector3 pos)
+    {
+        Instantiate(turret, new Vector3(pos.x, pos.y + 0.05f, pos.z), Quaternion.identity);
+    }
+}
